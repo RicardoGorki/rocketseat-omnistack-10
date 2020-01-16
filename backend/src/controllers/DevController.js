6b5b40bdc3ceb6ea19f353f31b0a11874bd10a68
RicardoGorki <ricardo.gorki@gmail.com>
2020-01-16 15:47:28 -0300

    fix api axios

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,6 +1,6 @@
-const api = require('../services/api');
 const Dev = require('../models/Dev');
 const parseStringAsArray = require('../utils/parseStringAsArray');
+const api = require('../services/api');
 
 module.exports = {
   async index(req, res) {
@@ -12,28 +12,32 @@ module.exports = {
   async store(req, res) {
     const { github_username, techs, latitude, longitude } = req.body;
 
-    let dev = await Dev.findOne({ github_username });
-    if (!dev) {
-      const response = await api.get(`/${github_username}`);
+    const devExists = await Dev.findOne({ github_username });
 
-      const { name = login, avatar_url, bio } = response.data;
-
-      const techsArray = parseStringAsArray(techs);
-
-      const location = {
-        type: 'Point',
-        coordinates: [longitude, latitude],
-      };
-
-      dev = await Dev.create({
-        github_username,
-        name,
-        avatar_url,
-        bio,
-        techs: techsArray,
-        location,
-      });
+    if (devExists) {
+      return res.status(401).json({ error: 'Dev already exists.' });
     }
-    return res.json(dev);
+
+    const response = await api.get(`${github_username}`);
+
+    const { name, avatar_url, bio } = response.data;
+
+    const techsArray = parseStringAsArray(techs);
+
+    const location = {
+      type: 'Point',
+      coordinates: [longitude, latitude],
+    };
+
+    await Dev.create(req.body);
+
+    return res.json({
+      github_username,
+      name,
+      avatar_url,
+      bio,
+      techs: techsArray,
+      location,
+    });
   },
 };
