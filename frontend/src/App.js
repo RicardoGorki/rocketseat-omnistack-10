0b939434c2a4f91ace5c78923dd4cf0ffb333607
RicardoGorki <ricardo.gorki@gmail.com>
2020-01-16 17:30:26 -0300

    frontend initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './styles/global.css';
+import './App.css';
+
+function App() {
+  return (
+    <div id="app">
+      <aside />
+      <main />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import props from 'prop-types';
+// import { Container } from './styles';
+
+function Header(props) {
+  return <h1>{props.title}</h1>;
+}
+
+export default Header;
