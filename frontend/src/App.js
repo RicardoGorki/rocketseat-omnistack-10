559131930d7892cdd10918b83c930d1fd301bc47
RicardoGorki <ricardo.gorki@gmail.com>
2020-01-19 12:37:14 -0300

    front done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,59 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
 import './styles/global.css';
 import './styles/App.css';
 import './styles/Sidebar.css';
 import './styles/Main.css';
 
+import DevForm from './components/DevForm';
+import DevItem from './components/DevItem';
+
 function App() {
-  navigator.geolocation.getCurrentPosition();
+  const [devs, setDevs] = useState([]);
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs');
+      setDevs(response.data);
+    }
+    loadDevs();
+  }, []);
+
+  async function handleAddDev(data) {
+    const response = await api.post('/devs', data);
+
+    setDevs([...devs, response.data]);
+  }
 
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input name="github_username" id="username_github" required />
-          </div>
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required />
-          </div>
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" required />
-            </div>
-
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required />
-            </div>
-          </div>
-
-          <button type="submit">Salvar</button>
-        </form>
+        <DevForm onSubmit={handleAddDev} />
       </aside>
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars1.githubusercontent.com/u/25038552?s=460&v=4"
-                alt="avatar"
-              />
-              <div className="user-info">
-                <strong>Ricardo Gorki</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>React</p>
-            <a href="https://github.com/ricardogorki">
-              Acessar perfil no Github
-            </a>
-          </li>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
         </ul>
       </main>
     </div>
